795c4cfc56d30e5f854410f04b1e35235a647835
TUSHAR SWAMI <tusharswami17@gamil.com>
2025-02-01 11:01:14 +0530

    add map on btn

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 
 const Calculator = () => {
+  const BUTTON_LIST = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/"
+  ]
+
   const [input, setInput] = useState("");
   const [result, setResult] = useState(0);
 
@@ -27,32 +31,14 @@ const Calculator = () => {
       <div className="bg-white p-8 rounded shadow-md w-full max-w-xs">
         <h1 className="text-2xl font-bold mb-4">Calculator</h1>
         <div className="mb-4">
-          <input
-            type="text"
-            value={input}
-            readOnly
-            className="w-full p-2 mb-2 border border-gray-300 rounded"
-          />
+          <input type="text" value={input} readOnly className="w-full p-2 mb-2 border !text-black border-gray-300 rounded" />
           <div className="grid grid-cols-4 gap-2">
-            <button name="1" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">1</button>
-            <button name="2" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">2</button>
-            <button name="3" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">3</button>
-            <button name="+" onClick={handleClick} className="p-2 bg-yellow-500 text-white rounded">+</button>
-            <button name="4" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">4</button>
-            <button name="5" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">5</button>
-            <button name="6" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">6</button>
-            <button name="-" onClick={handleClick} className="p-2 bg-yellow-500 text-white rounded">-</button>
-            <button name="7" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">7</button>
-            <button name="8" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">8</button>
-            <button name="9" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">9</button>
-            <button name="*" onClick={handleClick} className="p-2 bg-yellow-500 text-white rounded">*</button>
-            <button name="0" onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">0</button>
-            <button name="/" onClick={handleClick} className="p-2 bg-yellow-500 text-white rounded">/</button>
+            {BUTTON_LIST.map((button, i) => (<button key={i} name={button} onClick={handleClick} className="p-2 bg-blue-500 text-white rounded">{button}</button>))}
             <button onClick={clear} className="p-2 bg-red-500 text-white rounded">C</button>
             <button onClick={calculate} className="p-2 bg-green-500 text-white rounded">=</button>
           </div>
         </div>
-        <h2 className="text-xl font-bold">Result: {result === Infinity ? "can not divide by zero" : result}</h2>
+        <h2 className="text-xl font-bold !text-black">Result: {result === Infinity ? "can not divide by zero" : result}</h2>
       </div>
     </div>
   );
